59e037abaa58269a4d13456b79b84ed15e135053
Lucian Văcăroiu <silviulucian@gmail.com>
2020-03-06 12:48:43 +0200

    Moving dotfiles sync into separate script

---
+++ sync-dotfiles.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Sync dotfiles and secrets
+repos=( dotfiles secrets )
+for repo in "${repos[@]}"
+do
+  :
+  repo_dir="./$repo"
+  [[ ! -d $repo_dir ]] && git clone "git@github.com:silviulucian/$repo.git"
+  cd $repo_dir
+  git pull origin master
+  cd ..
+  rsync --exclude-from .rsyncignore -avh --no-perms $repo_dir/ ~
+done
+
+# Fix SSH permissions
+chmod 400 ~/.ssh/*
+chmod 600 ~/.ssh/config ~/.ssh/known_hosts
